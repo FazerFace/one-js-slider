07a5d1c7b90155bf2044c4b0544c1b926c6cd7cf
GoodBoy <51825024+FazerFace@users.noreply.github.com>
2020-01-08 19:17:45 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+﻿let btn_prev = document.querySelector('.hd_slider .hd_button .prev')
+let btn_next = document.querySelector('.hd_slider .hd_button .next')
+
+let slider = document.querySelectorAll('.hd_slider .hd_photo')
+let i = 0;
+
+btn_next.onclick = function(){
+    slider[i].style.display = 'none';
+    if(i >= 1){
+        i--;
+    }
+    slider[i].style.display = 'block';
+}
+
+btn_prev.onclick = function(){
+    slider[i].style.display = 'none';
+    if(i <= 1){
+        i++;
+    }
+    slider[i].style.display = 'block';
+}
+
+// TEST
+
